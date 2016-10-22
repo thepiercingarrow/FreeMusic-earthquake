21bac0df9b45501fa20071ce3e6b5419bb4434db
Maxwell Zhao <thepiercingarrow@gmail.com>
2016-10-21 21:40:08 -0400

    Fix splashscreen/icons and add hooks for icon/splashscreen generation

---
+++ scripts/copy-splashscreens.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+COMMAND() {
+    convert splashscreen.png -scale $1 splashscreens/splashscreen$1.png
+}
+mkdir -p splashscreens/
+COMMAND 320x480
+COMMAND 640x960
+COMMAND 768x1024
+COMMAND 1536x2048
+COMMAND 640x1136
+COMMAND 750x1334
+COMMAND 1242x2208
