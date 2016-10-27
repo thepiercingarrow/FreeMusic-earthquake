e20839861c3235cce37127862d16a623c3d71586
Maxwell Zhao <thepiercingarrow@gmail.com>
2016-10-27 01:09:46 -0400

    switch index.html to flex boxes to improve UI

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,7 @@
-document.addEventListener("deviceready", load, false);
+document.addEventListener("deviceready", load);
+
+document.getElementById('listen-link').addEventListener("click", e => window.location.replace('listen/index.html'));
+document.getElementById('download-link').addEventListener("click", e => window.location.replace('download.html'));
 
 function load() {
     if (StatusBar.isVisible) {
